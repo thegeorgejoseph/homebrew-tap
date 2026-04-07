90ebd3285b78c4f170e131b3e1bb34ae1a6fc8fb
george <thegeorgejoseph@gmail.com>
2026-04-06 23:04:15 -0700

    mico 0.1.5

--- Formula/mico.rb
+++ Formula/mico.rb
@@ -1,9 +1,9 @@
 class Mico < Formula
   desc "Mission control for local CLI AI agents"
   homepage "https://github.com/thegeorgejoseph/mico"
-  url "https://github.com/thegeorgejoseph/mico/releases/download/v0.1.4/mico-0.1.4-aarch64-apple-darwin.tar.gz"
-  sha256 "b018766d3ace056f72825ad19760377f5e209868d37170c15906405252c5d5ba"
-  version "0.1.4"
+  url "https://github.com/thegeorgejoseph/mico/releases/download/v0.1.5/mico-0.1.5-aarch64-apple-darwin.tar.gz"
+  sha256 "d678091a7f4cb36930b56a390f4d595e5a5b2610e4e443152f7de0b90f9a70ba"
+  version "0.1.5"
 
   depends_on "tmux"
 
